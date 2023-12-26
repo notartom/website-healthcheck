4bd25ddcfd3f3d084885f7a106c4d35c8123535a
Artom Lifshitz <alifshit@redhat.com>
2023-12-25 21:15:11 -0500

    Add insecure option
    This option, defaulted to false, passes `-k` to curl and allows it to
    work in cases where the website being checked is deployed with
    self-signed certificates in CI.

--- scan-website.sh
+++ scan-website.sh
@@ -1,6 +1,11 @@
 website_url=$1
 scan_text=$2
-status_code=$(curl --silent --location --head --output /dev/null --write-out "%{http_code}" "${website_url}")
+insecure=$3
+if [ $insecure == "true" ]
+then
+    status_code=$(curl -k --silent --location --head --output /dev/null --write-out "%{http_code}" "${website_url}")
+else
+    status_code=$(curl --silent --location --head --output /dev/null --write-out "%{http_code}" "${website_url}")
 
 if [ "${status_code}" != "200" ]; then
     echo "Website is unreachable (${status_code})" >&2
